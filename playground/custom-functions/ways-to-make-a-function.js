cc318b0548aa640586b3f5bbe9ae015dbaa9cb3d
Janez Kolar <kolarjanez@yahoo.com>
2020-04-25 16:22:58 +0200

    Added functions examples

---
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -0,0 +1,16 @@
+/* function doctorize(firstName) {
+  return `Dr. ${firstName}`;
+} */
+
+/* // Anon Function
+function (firstName) {
+    return `Dr. ${firstName}`;
+} */
+
+/* // Function Expression
+const doctorize = function(firstName) {
+  doesntExist();
+  return `Dr. ${firstName}`;
+}; */
+
+const inchToCM = inches => inches * 2.54;
