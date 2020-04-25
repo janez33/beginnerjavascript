e3efa7be44cbdea5277b0d97efcafc57fceacc2c
Janez Kolar <kolarjanez@yahoo.com>
2020-04-25 10:29:14 +0200

    Commit after finishing custom functions video

---
+++ some.js
@@ -0,0 +1,11 @@
+const p = document.querySelector('p');
+console.log(p);
+/* eslint-disable */
+const name = 'janez';
+const middle = "topher";
+const toast = `bos`;
+const sentence = 'shes\'s so cool';
+
+/* const hello = "hello my name is " + name + ". Nice to meet you"; */
+
+const hello = `hello my name is ${name}. Nice to meet you. lala`
