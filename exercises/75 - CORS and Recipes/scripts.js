86e5af0bc07c69024104abbb58b109e70e92541b
Johnny <kolarjanez@yahoo.com>
2023-04-23 22:18:02 +0200

    Finished CORS and Recipes (exercise 75)

---
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -0,0 +1,48 @@
+// The Recipe Puppy API used in the course is broken
+// Please use this replacement API URL "https://recipes.beginnerjavascript.com/api"
+const baseEndpoint = 'https://recipes.beginnerjavascript.com/api';
+const proxy = `https://corsproxy.io/?`;
+const form = document.querySelector('form.search');
+const recipesGrid = document.querySelector('.recipes');
+
+async function fetchRecipes(query) {
+  const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
+  const data = await res.json();
+  return data;
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const el = event.currentTarget;
+  console.log(form.query.value);
+  fetchAndDisplay(form.query.value);
+}
+
+async function fetchAndDisplay(query) {
+  // turn the form off
+  form.submit.disabled = true;
+  // submit the search
+  const recipes = await fetchRecipes(query);
+  console.log(recipes);
+  form.submit.disabled = false;
+  displayRecipes(recipes.results);
+}
+
+function displayRecipes(recipes) {
+  console.log('Creating HTML');
+  const html = recipes.map(
+    recipe => `
+    <div class="recipe">
+      <h2>${recipe.title}</h2>
+      <p>${recipe.ingredients}</p>
+      ${recipe.thumbnail &&
+        `<img class="recipe-image" width="100px" src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
+      <a href="{$recipe.href}">View Recipe &#10140;</a>
+    </div>`
+  );
+  recipesGrid.innerHTML = html.join('');
+}
+
+form.addEventListener('submit', handleSubmit);
+// on page load run it with pizza
+fetchAndDisplay('pizza');
